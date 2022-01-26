5f021b759b03f7b93419d8ee9d786092a2ea4031
Corbin Davenport <me@corbin.io>
2022-01-26 00:53:02 -0500

    Add --force-rebuild flag to compiler

--- compile.sh
+++ compile.sh
@@ -1,8 +1,39 @@
 #!/bin/bash
 
+function get_dependencies {
+  # Delete tmp folder if already present
+  rm -rf "$DIR/tmp"
+  # Delete system bin folder if needed
+  rm -rf "$DIR/sysroot/system/bin"
+  # Delete source zip folder if needed
+  rm -f "$DIR/cdroot/source.zip"
+  # Start downloading dependencies
+  mkdir -p "$DIR/tmp"
+  while IFS="" read -r p || [ -n "$p" ]; do
+    # Split package name
+    IFS="/" 
+    read -a PACKAGE <<< "$p"
+    # Download
+    echo "Downloading package: $p"
+    curl --progress-bar -A "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0" -o "$DIR/tmp/${PACKAGE[1]}.zip" "https://www.ibiblio.org/pub/micro/pc-stuff/freedos/files/repositories/latest/$p.zip"
+    # Create single BIN folder from all zip files
+    unzip -o "$DIR/tmp/${PACKAGE[1]}.zip" "BIN/*" "bin/*" -x "*/_*" "*/*.HLP" -d "$DIR/tmp/"
+    # Create source folder
+    unzip -o "$DIR/tmp/${PACKAGE[1]}.zip" "SOURCE/*" "source/*" -x "*/_*" -d "$DIR/tmp/"
+  done < "$DIR/packages.txt"
+  # Zip up combined source folder and move it to the CD
+  cd "$DIR/tmp/SOURCE"
+  zip -r "$DIR/cdroot/source.zip" *
+  cd "$DIR"
+  # Move binaries to system folder
+  mkdir -p "$DIR/sysroot/system/bin"
+  cp -f -r tmp/BIN/* sysroot/system/bin
+}
+
 DIR="$(pwd "${BASH_SOURCE[0]}")"
 PUBLISHER="Cobalt"
 TITLE="Cobalt Live CD"
+PARAM=$1
 
 cd "$DIR"
 
@@ -24,27 +55,12 @@ mkdir -p "$DIR/dist"
 # Download packages
 # This is disabled for now, add a '!' after 'if' to test
 
-if ! [ -d "$DIR/tmp" ]; then
-  mkdir -p "$DIR/tmp"
-  while IFS="" read -r p || [ -n "$p" ]; do
-    # Split package name
-    IFS="/" 
-    read -a PACKAGE <<< "$p"
-    # Download
-    echo "Downloading package: $p"
-    curl --progress-bar -A "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0" -o "$DIR/tmp/${PACKAGE[1]}.zip" "https://www.ibiblio.org/pub/micro/pc-stuff/freedos/files/repositories/latest/$p.zip"
-    # Create single BIN folder from all zip files
-    unzip -o "$DIR/tmp/${PACKAGE[1]}.zip" "BIN/*" "bin/*" -x "*/_*" "*/*.HLP" -d "$DIR/tmp/"
-    # Create source folder
-    unzip -o "$DIR/tmp/${PACKAGE[1]}.zip" "SOURCE/*" "source/*" -x "*/_*" -d "$DIR/tmp/"
-  done < "$DIR/packages.txt"
-  # Zip up combined source folder and move it to the CD
-  cd "$DIR/tmp/SOURCE"
-  zip -r "$DIR/cdroot/source.zip" *
-  cd "$DIR"
-  # Move binaries to system folder
-  mkdir -p "$DIR/sysroot/system/bin"
-  cp -f -r tmp/BIN/* sysroot/system/bin
+if [ "$PARAM" == "--force-rebuild" ]; then
+  get_dependencies
+elif ! [ -d "$DIR/tmp" ]; then
+  get_dependencies
+else
+  echo "Dependencies already downloaded, skipping..."
 fi
 
 # Save build date to CD
--- docs/build.md
+++ docs/build.md
@@ -1,10 +1,18 @@
 # Building and testing Cobalt
 
-Clone the repository, then run the compile script. You need `mkisofs` installed and a Linux host.
+Clone the repository to a Linux host, then run the compile script:
+
 ```
 ./compile.sh
 ```
+
 This will generate a bootable ISO image at `dist/cobalt.iso`. This can be written to a physical CD/USB or used with a virtual machine.
+
+This process involves downloading FreeDOS packages listed in `packages.txt` and placing them in the `tmp/` folder. If you modify the list and need to refresh the downloaded packages, run the script with the flag:
+
+```
+./compile.sh --force-rebuild
+```
 
 If you clone the repository, you can also compile an ISO with GitHub Actions by navigating to `Actions > Build Nightly > Run workflow`.
 
